6bdf48f952315531e3b8d9ec80b23ce9c0622f5e
sphisi <143594972+sphisi@users.noreply.github.com>
2025-05-08 00:35:35 +0000

    god

--- video1.py
+++ video1.py
@@ -1,16 +1,34 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+    
+    def __str__(self):
+        return f"{self.name} from {self.house} "
+    
+    #getter
+    def house(self):
+        return self.house
+    
+    #setter
+    def house(self, house):
+        if house not in
+        self.house = house
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    student.house = "no way jose"
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("house: ")
-    student = Student(name, house)
-    return student
+    return Student(name, house)
 
 
 if __name__ == "__main__":
